f3ad9672ded48bde4d206a9c0e77feb69bdaf142
Lin Lin Hlaing <linlinhlaing@Lins-Air.lan>
2024-09-20 17:13:21 -0500

    fixed server error due to json format issues

--- static/app.js
+++ static/app.js
@@ -1,4 +1,3 @@
-
 // Clear the input and result boxes
 function clearFields() {
     document.getElementById('text-input').value = '';  // Clear the text input
@@ -10,3 +9,31 @@ function clearFields() {
 document.getElementById('clear-btn').addEventListener('click', clearFields);
 
 document.getElementById('detect-btn').addEventListener('click', classifyText);
+
+function classifyText() {
+    const inputText = document.getElementById('text-input').value;
+
+    fetch('/classify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 'text-input': inputText })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle the response data
+        const resultBox = document.getElementById('result-box');
+        const encoding = data.result;
+        resultBox.innerHTML = '<span class="small-text">Your input is: </span><span class="' + (encoding === 'Unicode' ? 'unicode' : 'zawgyi') + '">' + encoding + '</span>';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('result-box').textContent = 'Error: ' + error.message;
+    });
+}
